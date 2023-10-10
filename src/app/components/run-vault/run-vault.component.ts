f76a1e0029464a2d02e1dd993d611d7407da04e3
alex shpurov <alextmn1@gmail.com>
2023-10-10 12:59:57 -0400

    layout

--- src/app/components/run-vault/run-vault.component.ts
+++ src/app/components/run-vault/run-vault.component.ts
@@ -6,8 +6,19 @@ import { firstValueFrom } from 'rxjs';
 
 interface ParamDesc {
   name: string
-  isEncrypted: boolean
+  isEncrypted: boolean,
+  dimensions: number[] ,
+  i: number,
+  j: number,
 }
+
+interface ClientSpecsInput {
+  encryption: 
+    { secretKeyID: number }
+  shape:
+  { dimensions: number[] }
+}
+
 @Component({
   selector: 'app-run-vault',
   templateUrl: './run-vault.component.html',
@@ -16,7 +27,7 @@ interface ParamDesc {
 export class RunVaultComponent implements OnInit{
   paramsForm =new UntypedFormGroup({});
   id: any;
-  paramsDesc: boolean[] = [];
+  paramsDesc: ParamDesc[] = [];
   stage = -1
   result = ''
 
@@ -35,43 +46,68 @@ export class RunVaultComponent implements OnInit{
   get fval() { return this.paramsForm.controls; }
 
   async load_specs() {
-    const resp = await firstValueFrom(
-      this.http.post<any>(`vault/client-specs/${this.id}`, {})) 
-    const inputs = resp['inputs'];
-    let  i = 0;
+    const resp_b64 = await firstValueFrom(
+      this.http.post<any>(`vault/client-specs/${this.id}`, {}))
     
-    console.log(resp)
+    console.log('client-specs resp_b64:', resp_b64)
 
-    const paramsDesc: boolean[] = inputs
-    .map((a: { [x: string]: any; }) => a['encryption']?? "-1")
-    .map((a: { [x: string]: any; }) => a['secretKeyID'] ?? "-1")
-    .map((a: string) => a !== "-1");
+    const inputs = JSON.parse(atob(resp_b64[0]))['inputs'];
+    console.log('client-specs:', inputs)
+
+    const paramsDesc: ParamDesc[] = inputs
+        .map((a: ClientSpecsInput, i: number) => {
+          const encryption = a.encryption?.secretKeyID ?? -1;
+          const dimensions = a.shape?.dimensions ?? [];
+          const length: number = dimensions.reduce((a,b) => a * b, 1);
+          if (length > 1) {
+            return Array.from({ length }, (_, j) => 
+              <ParamDesc>{ name: `Param ${i}_${j}`, isEncrypted: encryption !== -1, dimensions, i, j } )
+          }
+          return { name: `Param ${i}`, isEncrypted: encryption !== -1, dimensions, i, j: 0 }
+        }).flat()
+
+    console.log('paramsDesc', paramsDesc)
 
     const params: {[key: string]: any;} = {"password": new FormControl("xxx")};
     
     paramsDesc.forEach(a=> {
-      params[`param${++i}`] = new FormControl("0")
+      params[a.name] = new FormControl("0")
     })
-
-    console.log('params',params, paramsDesc)
-    
+   
     this.paramsForm = new FormGroup(params);
     this.paramsDesc = paramsDesc;
    
   }
 
   async encryptRunDecrypt(): Promise<string[]> {
-    this.stage = -1;
-    this.result = ''
     try {
-      const password = this.fval['password'].value;
-      const values = this.paramsDesc.map((a,i) => this.fval[`param${++i}`].value);
+      const last: number[] = this.paramsDesc.map(a => a.i).slice(-1);
+      const length = last.length > 0 ? last[0] + 1 : 0;
+
+      const values = Array.from({length }, (_, i) => {
+        const p = this.paramsDesc.find(a=> a.i === i) !!; 
+        if (p.dimensions.length > 0) {
+          return  this.paramsDesc
+            .filter(b=> b.i === i)
+            .map(b => +this.fval[b.name].value)
+        }
+        return +this.fval[p.name].value;
+      })
+
+      console.log('collected values:', JSON.stringify(values))
+
+      this.stage = -1;
+      this.result = ''
+
+      const password = 'not-set';
+      const clientSpecs = await firstValueFrom(
+        this.http.post<any>(`vault/client-specs/${this.id}`, {})) 
       this.stage = 0;
-      const encrypted = await this.encrypt(values, password);
+      const encrypted = await this.encrypt(values, clientSpecs[0], password);
       this.stage = 1;
       const executed = await this.runCircuit(encrypted);
       this.stage = 2;
-      const decrypted = await this.decrypt(executed, password);
+      const decrypted = await this.decrypt(executed, clientSpecs[0], password);
       this.result = decrypted;
       return decrypted;
     } finally {
@@ -79,9 +115,9 @@ export class RunVaultComponent implements OnInit{
     }
   }
   
-  async encrypt(values: string[], password: string): Promise<string[]> {
+  async encrypt(values: any, client_specs: any, password: string): Promise<string[]> {
     const resp = await firstValueFrom(
-      this.http.post<any>(`vault/encrypt/${this.id}`, {values})) 
+      this.http.post<any>(`vault/encrypt/${this.id}`, {values, client_specs})) 
     console.log('encrypt',resp)
     return resp;
   }
@@ -96,9 +132,9 @@ export class RunVaultComponent implements OnInit{
     return resp;
   }
 
-  async decrypt(encryptedValues: string[], password: string) {
+  async decrypt(encryptedValues: string[],  client_specs: any, password: string) {
     const respDescrypted = await firstValueFrom(
-      this.http.post<any>(`vault/decrypt/${this.id}`, {values: encryptedValues[0]})) 
+      this.http.post<any>(`vault/decrypt/${this.id}`, {values: encryptedValues[0], client_specs })) 
 
       console.log('respDescrypted:', respDescrypted)
       return respDescrypted;
